ec42913a35ff0906afd2c99033950f5bf8fe6f31
BlackAndWhiteScallion <tanguo@hotmail.com>
2023-08-23 14:19:45 +0800

    replace

---
+++ dist/assets/AboutView.97f86836.js
@@ -0,0 +1,1 @@
+import{n}from"./index.f3af7628.js";const t={};var s=function(){var e=this;return e._self._c,e._m(0)},a=[function(){var _=this,e=_._self._c;return e("div",{staticClass:"about"},[e("h1",[_._v("This is an about page")])])}],c=n(t,s,a,!1,null,"ecd8c565",null,null);const r=c.exports;export{r as default};
