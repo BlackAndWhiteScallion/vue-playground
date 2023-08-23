ad6c4200c911d156b20d96e00e69f118eaba4164
BlackAndWhiteScallion <tanguo@hotmail.com>
2023-08-23 11:24:29 +0800

    up

--- docs/assets/AboutView.86856ecb.js
+++
@@ -1,1 +0,0 @@
-import{n}from"./index.3351c86f.js";const t={};var s=function(){var e=this;return e._self._c,e._m(0)},a=[function(){var _=this,e=_._self._c;return e("div",{staticClass:"about"},[e("h1",[_._v("This is an about page")])])}],c=n(t,s,a,!1,null,"ecd8c565",null,null);const r=c.exports;export{r as default};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    baseUrl: "/vue-playground/",
+    baseUrl: "",
     publicPath: './'
   }
